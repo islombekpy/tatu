ce22805adc3dca9a164117860e6fd6cf65c44b83
islombek-develober <islombekbotiraliyev1@gmail.com>
2025-03-21 16:10:37 +0500

    edit views

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-import os 
+import json
 
-json_path = os.path.join("questions.json")
-print(json_path)
+with open('quiz.txt', 'r', encoding='utf-8') as file:
+    text = file.read()
+
+questions = []
+current_question = None
+
+for line in text.split("\n"):
+    line = line.strip()
+    if line.startswith("++++"):
+        if current_question:
+            questions.append(current_question)
+        current_question = {"question": line[5:].strip(), "answer": ""}
+    elif line.startswith("==== #") and current_question:
+        current_question["answer"] = line[6:].strip()
+
+if current_question and current_question["question"] and current_question["answer"]:
+    questions.append(current_question)
+
+with open('questions.json', 'w', encoding='utf-8') as json_file:
+    json.dump(questions, json_file, ensure_ascii=False, indent=4)
+
+print("JSON file saved as questions.json")
